2a6e35514a7ce6fc566b6722c30245d884b37b35
DHANESHJOSHI <hydradhanesh@gmail.com>
2024-07-17 18:28:42 +0530

    updated

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps : [{
+      name: "next-app",
+      script: "npm",
+      args: "start",
+      cwd: "/home/ubuntu/DipanshuCsrBox/",
+      watch: true,
+      env: {
+        NODE_ENV: "production",
+      }
+    }]
+  };
+  
